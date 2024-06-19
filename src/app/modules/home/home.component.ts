2bf35b99aef5643232b808218d47768a911508df
João Gabriel Carvalho <27.joaogabriel@gmail.com>
2024-06-19 19:05:00 -0300

    feat: added implementation of forms, sign in and sign up

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  constructor(private formBuilder: FormBuilder) { }
+
   loginCard: boolean = true
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  signUpForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  public onSubmitLoginForm(): void {
+    alert(this.loginForm.value)
+  }
+
+  public onSubmitSignUpForm(): void {
+    alert(this.signUpForm.value)
+  }
 }
